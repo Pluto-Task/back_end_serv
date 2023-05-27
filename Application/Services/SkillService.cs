0f3d5ed8b50d2361f79baa97153336aaaaab840e
Bohdan Fylypchuk <fylypchukb@gmail.com>
2023-05-27 20:49:04 +0300

    Enum in ukrainian

--- Application/Services/SkillService.cs
+++ Application/Services/SkillService.cs
@@ -1,4 +1,6 @@
-﻿using Application.Abstractions;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using Application.Abstractions;
 using Domain.Enums;
 using Domain.Shared;
 
@@ -8,6 +10,26 @@ public class SkillService : ISkillService
 {
     public Result<IEnumerable<string>> GetAllSkillStrings(CancellationToken cancellationToken)
     {
-        return Enum.GetNames<SkillName>();
+        return Enum.GetNames<SkillName>().Select(GetDisplayName).ToList();
+    }
+
+    private static string GetDisplayName(string name)
+    {
+        var type = typeof(SkillName);
+
+        var fieldInfo = type.GetField(name);
+
+        if (fieldInfo != null)
+        {
+            if (Attribute.GetCustomAttribute(fieldInfo, typeof(DisplayAttribute)) is DisplayAttribute
+                {
+                    Name: not null
+                } attribute)
+            {
+                return attribute.Name;
+            }
+        }
+
+        return name;
     }
 }
--- Domain/Enums/SkillName.cs
+++ Domain/Enums/SkillName.cs
@@ -4,19 +4,48 @@ namespace Domain.Enums;
 
 public enum SkillName
 {
+    [Display(Name = "Психологічна допомога")]
     PsychologicalAssistance = 1,
+
+    [Display(Name = "Медична допомога")]
     MedicalCare,
+
+    [Display(Name = "Догляд за хворими")]
     NursingCare,
+
+    [Display(Name = "Догляд за дітьми")]
     ChildCare,
+
+    [Display(Name = "Медичні тренінги")]
     MedicalTraining,
+
+    [Display(Name = "Військові тренінги")]
     MilitaryTraining,
+
+    [Display(Name = "Перевезення вантажів")]
     TransportGoods,
+
+    [Display(Name = "Перевезення людей")]
     TransportPeople,
+
+    [Display(Name = "Ремонт техніки")]
     RepairEquipment,
+
+    [Display(Name = "Приготування їжі")]
     FoodPreparation,
+
+    [Display(Name = "Переклади")]
     Translation,
+
+    [Display(Name = "Пошиття одягу")]
     SewingClothes,
+
+    [Display(Name = "Плетіння сіток")]
     WeavingNets,
+
+    [Display(Name = "Допомога на складі")]
     HelpWarehouse,
+
+    [Display(Name = "Розбір завалів")]
     DebrisRemoval
 }
